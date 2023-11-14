f5b61a7cbbfee1ef95f03b2de4fd7143b36a1c42
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-11-14 19:53:38 +0900

    Adjust popup-calendar for high DPI

--- polybar-scripts/popup-calendar/popup-calendar.sh
+++ polybar-scripts/popup-calendar/popup-calendar.sh
@@ -1,34 +1,38 @@
 #!/bin/sh
 
-BAR_HEIGHT=22  # polybar height
-BORDER_SIZE=1  # border size from your wm settings
+DPI_SCALE=2    # your dpi / 96
+BAR_HEIGHT=32  # polybar height
+BORDER_SIZE=5  # border size from your wm settings
 YAD_WIDTH=222  # 222 is minimum possible value
 YAD_HEIGHT=193 # 193 is minimum possible value
-DATE="$(date +"%a %d %H:%M")"
+DATE="$(date +"%Y-%m-%d %H:%M")"
 
 case "$1" in
 --popup)
     if [ "$(xdotool getwindowfocus getwindowname)" = "yad-calendar" ]; then
-        exit 0
+      killall -9 yad
+      exit 0
     fi
 
     eval "$(xdotool getmouselocation --shell)"
-    eval "$(xdotool getdisplaygeometry --shell)"
+    screen_res=$(xdpyinfo | awk '/dimensions:/ {print $2}')
+    WIDTH=$(echo $screen_res | sed -e's/x.*//')
+    HEIGHT=$(echo $screen_res | sed -e's/.*x//')
 
     # X
-    if [ "$((X + YAD_WIDTH / 2 + BORDER_SIZE))" -gt "$WIDTH" ]; then #Right side
-        : $((pos_x = WIDTH - YAD_WIDTH - BORDER_SIZE))
-    elif [ "$((X - YAD_WIDTH / 2 - BORDER_SIZE))" -lt 0 ]; then #Left side
-        : $((pos_x = BORDER_SIZE))
+    if [ "$((X + DPI_SCALE * YAD_WIDTH / 2 + DPI_SCALE * BORDER_SIZE))" -gt "$WIDTH" ]; then #Right side
+        : $((pos_x = WIDTH - DPI_SCALE * YAD_WIDTH - DPI_SCALE * BORDER_SIZE))
+    elif [ "$((X - DPI_SCALE * YAD_WIDTH / 2 - DPI_SCALE * BORDER_SIZE))" -lt 0 ]; then #Left side
+        : $((pos_x = DPI_SCALE * BORDER_SIZE))
     else #Center
-        : $((pos_x = X - YAD_WIDTH / 2))
+        : $((pos_x = X - DPI_SCALE * YAD_WIDTH / 2))
     fi
 
     # Y
     if [ "$Y" -gt "$((HEIGHT / 2))" ]; then #Bottom
-        : $((pos_y = HEIGHT - YAD_HEIGHT - BAR_HEIGHT - BORDER_SIZE))
+        : $((pos_y = HEIGHT - DPI_SCALE * YAD_HEIGHT - DPI_SCALE * BAR_HEIGHT - DPI_SCALE * BORDER_SIZE))
     else #Top
-        : $((pos_y = BAR_HEIGHT + BORDER_SIZE))
+        : $((pos_y = DPI_SCALE * BAR_HEIGHT + DPI_SCALE * BORDER_SIZE))
     fi
 
     yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons \
